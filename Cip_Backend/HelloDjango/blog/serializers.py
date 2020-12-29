4fd816197a370a3fd9030818b6d01932e3a81be8
Хайбулов Эльдар <64076865+Eldar1988@users.noreply.github.com>
2020-12-29 17:02:58 +0600

    + post serializers

---
+++ Cip_Backend/HelloDjango/blog/serializers.py
@@ -0,0 +1,40 @@
+from rest_framework import serializers
+from .models import Category, Post, Comment, PostPhoto
+
+
+class PostsListSerializer(serializers.ModelSerializer):
+    """Posts list"""
+    class Meta:
+        model = Post
+        exclude = ('body', 'future', 'public', 'order', 'update')
+
+
+class CategoriesSerializer(serializers.ModelSerializer):
+    """Post categories"""
+    class Meta:
+        model = Category
+        exclude = ('order',)
+
+
+class CommentsSerializer(serializers.ModelSerializer):
+    """Post comments"""
+    class Meta:
+        model = Comment
+        exclude = ('public',)
+
+
+class PostPhotosSerializer(serializers.ModelSerializer):
+    """Post photos"""
+    class Meta:
+        model = PostPhoto
+        exclude = ('post', 'order', 'pub_date', 'update')
+
+
+class PostDetailSerializer(serializers.ModelSerializer):
+    """Post detail"""
+    comments = CommentsSerializer(many=True)
+    images = PostPhotosSerializer(many=True)
+
+    class Meta:
+        model = Post
+        exclude = ('public', 'future', 'order', 'update')
